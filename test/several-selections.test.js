6fa5c241bb03dcccc430986e5a52246bb075a281
Jason Lenoble <jason.lenoble@gmail.com>
2018-07-31 02:31:12 +0200

    Passing shared item deletion

--- test/several-selections.test.js
+++ test/several-selections.test.js
@@ -44,4 +44,40 @@ describe('Testing several selections', function () {
 
     expect(UI.refCount).to.equal(9);
   });
+
+  it(`Deleting a shared item`, function () {
+    const {UI, itemIds} = this;
+    const [id1, id2, id3, id4, id5] = itemIds;
+
+    const sel1 = UI.newSelection({
+      title: 'A',
+      itemIds: [id1, id4],
+    });
+
+    const sel2 = UI.newSelection({
+      title: 'B',
+      itemIds: [id1, id5, id2],
+    });
+
+    const sel3 = UI.newSelection({
+      title: 'C',
+      itemIds: [id3, id1, id5, id4],
+    });
+
+    expect(UI.refCount).to.equal(9);
+
+    UI.destroyItem(id5);
+
+    expect(UI.refCount).to.equal(7);
+    expect(sel1.itemIds).to.eql([id1, id4]);
+    expect(sel2.itemIds).to.eql([id1, id2]);
+    expect(sel3.itemIds).to.eql([id3, id1, id4]);
+
+    UI.destroyItem(id1);
+
+    expect(UI.refCount).to.equal(4);
+    expect(sel1.itemIds).to.eql([id4]);
+    expect(sel2.itemIds).to.eql([id2]);
+    expect(sel3.itemIds).to.eql([id3, id4]);
+  });
 });
