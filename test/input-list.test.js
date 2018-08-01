991f3233d8728b343a62a96976526e2215e7dbff
Jason Lenoble <jason.lenoble@gmail.com>
2018-08-01 13:37:57 +0200

    Don't use title as id in tests

--- test/input-list.test.js
+++ test/input-list.test.js
@@ -6,8 +6,10 @@ import InputList from '../src/input-list';
 
 describe(`Class InputList`, function () {
   beforeEach(function () {
+    let id = 0;
     const Model = class {
       constructor ({title}) {
+        this.id = ++id;
         this.title = title;
       }
     };
@@ -15,20 +17,20 @@ describe(`Class InputList`, function () {
     const o1 = new Model({title: 'a'});
     const o2 = new Model({title: 'b'});
     const o3 = new Model({title: 'c'});
-    const o4 = new Model({title: 'd'});
 
     const objs = [o1, o2, o3];
 
-    const selection = new Selection(objs.map(o => [o.title, o]));
+    const selection = new Selection(objs.map(o => [o.id, o]));
     const inputList = new InputList({
-      Model, idProperty: 'title', textProperty: 'title', selection,
+      Model, idProperty: 'id', textProperty: 'title', selection,
     });
 
-    Object.assign(this, {Model, o1, o2, o3, o4, objs, selection, inputList});
+    Object.assign(this, {Model, o1, o2, o3, objs, selection, inputList});
   });
 
   it(`Input a text`, function () {
-    const {o4, objs, selection, inputList} = this;
+    const {objs, selection, inputList} = this;
+    const o4 = {id: 4, title: 'd'};
 
     inputList.add(o4.title);
 
@@ -39,7 +41,7 @@ describe(`Class InputList`, function () {
   it(`Remove an item`, function () {
     const {o1, objs, selection, inputList} = this;
 
-    inputList.remove(o1.title);
+    inputList.remove(o1.id);
 
     expect(Array.from(inputList.values())).to.eql(objs.slice(1));
     expect(Array.from(selection.values())).to.eql(objs.slice(1));
