c041ae148af77629bf3491c558d404e0084201f8
Jason Lenoble <jason.lenoble@gmail.com>
2018-08-02 19:25:40 +0200

    Wrapped test suites with describe
    prevent beforeEach from bleeding over following tests

--- test/helpers/test-suites/input-test-suite.js
+++ test/helpers/test-suites/input-test-suite.js
@@ -5,30 +5,32 @@ import {Input} from '../../../src/ui';
 import {setup} from '../setup';
 
 export function inputTestSuite ({Selection, idProperty, inputProperty} = {}) {
-  beforeEach(function () {
-    setup.call(this, {Selection, idProperty, inputProperty});
-    const {Model, selection} = this;
+  describe(`Implements a generic CRUD`, function () {
+    beforeEach(function () {
+      setup.call(this, {Selection, idProperty, inputProperty});
+      const {Model, selection} = this;
 
-    this.input = new Input({Model, idProperty, inputProperty, selection});
-  });
+      this.input = new Input({Model, idProperty, inputProperty, selection});
+    });
 
-  it(`Input a text`, function () {
-    const {u1, u2, u3, u4, u5, selection, input} = this;
-    input.create(u4[inputProperty]);
-    input.create(u5[inputProperty]);
-    expect(Array.from(selection.values())).to.eql([u1, u2, u3, u4, u5]);
-  });
+    it(`Input a text`, function () {
+      const {u1, u2, u3, u4, u5, selection, input} = this;
+      input.create(u4[inputProperty]);
+      input.create(u5[inputProperty]);
+      expect(Array.from(selection.values())).to.eql([u1, u2, u3, u4, u5]);
+    });
 
-  it(`Delete an item`, function () {
-    const {u1, u2, u3, selection, input} = this;
-    input.delete(u1[idProperty]);
-    expect(Array.from(selection.values())).to.eql([u2, u3]);
-  });
+    it(`Delete an item`, function () {
+      const {u1, u2, u3, selection, input} = this;
+      input.delete(u1[idProperty]);
+      expect(Array.from(selection.values())).to.eql([u2, u3]);
+    });
 
-  it(`Edit an item`, function () {
-    const {u1, u2, u3, selection, input} = this;
-    const O1 = {[idProperty]: u1[idProperty], [inputProperty]: 'A'};
-    input.update(u1[idProperty], O1[inputProperty]);
-    expect(Array.from(selection.values())).to.eql([O1, u2, u3]);
+    it(`Edit an item`, function () {
+      const {u1, u2, u3, selection, input} = this;
+      const O1 = {[idProperty]: u1[idProperty], [inputProperty]: 'A'};
+      input.update(u1[idProperty], O1[inputProperty]);
+      expect(Array.from(selection.values())).to.eql([O1, u2, u3]);
+    });
   });
 }
