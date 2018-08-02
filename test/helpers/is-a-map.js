b4540c655f973bb0da013154a7b760ba95e97d99
Jason Lenoble <jason.lenoble@gmail.com>
2018-08-02 14:10:27 +0200

    Grouped tests, added checks on return values

--- test/helpers/is-a-map.js
+++ test/helpers/is-a-map.js
@@ -7,74 +7,48 @@ export const isAMap = Type => {
     beforeEach(function () {
       const o1 = {title: 'a'};
       const o2 = {title: 'b'};
+      const o3 = {title: 'c'};
       const map = new Type([[o1.title, o1], [o2.title, o2]]);
 
-      Object.assign(this, {map, o1, o2});
+      Object.assign(this, {map, o1, o2, o3});
     });
 
     it(`ctor()`, function () {
-      const map = new Type();
-      expect(map.size).to.equal(0);
-    });
+      const map0 = new Type();
+      expect(map0.size).to.equal(0);
 
-    it(`ctor([[o1.title, o1], [o2.title, o2]]])`, function () {
       const {map, o1, o2} = this;
       expect(map.size).to.equal(2);
       expect(Array.from(map.values())).to.eql([o1, o2]);
     });
 
-    it(`set(o1.title, o1)`, function () {
-      const {map, o1, o2} = this;
-      map.set(o1.title, o1);
+    it(`set()`, function () {
+      const {map, o1, o2, o3} = this;
+      expect(map.set(o1.title, o1)).to.equal(map);
       expect(Array.from(map.values())).to.eql([o1, o2]);
-    });
 
-    it(`set(o1.title, o1).set(o2.title, o2)`, function () {
-      const {map, o1, o2} = this;
-      expect(() => map.set(o1.title, o1).set(o2.title, o2)).not.to.throw();
-      expect(Array.from(map.values())).to.eql([o1, o2]);
-    });
-
-    it(`set(o1.title, o1).set(o2.title, o2).set(o3.title, o3)`, function () {
-      const {map, o1, o2} = this;
-      const o3 = {title: 'c'};
-      map.set(o1.title, o1).set(o2.title, o2).set(o3.title, o3);
+      expect(map.set(o2.title, o2).set(o3.title, o3)).to.equal(map);
       expect(Array.from(map.values())).to.eql([o1, o2, o3]);
     });
 
-    it(`delete(o1.title)`, function () {
-      const {map, o1, o2} = this;
-      map.delete(o1.title);
-      expect(Array.from(map.values())).to.eql([o2]);
-    });
-
-    it(`delete(o1.title), delete(o2.title)`, function () {
-      const {map, o1, o2} = this;
-      map.delete(o1.title);
-      map.delete(o2.title);
+    it(`delete()`, function () {
+      const {map, o1, o2, o3} = this;
+      expect(map.delete(o1.title)).to.be.true;
+      expect(map.delete(o2.title)).to.be.true;
       expect(Array.from(map.values())).to.eql([]);
+      expect(map.delete(o3.title)).to.be.false;
     });
 
-    it(`delete(o1.title), delete(o2.title), delete(o3.title)`, function () {
-      const {map, o1, o2} = this;
-      const o3 = {title: 'c'};
-      map.delete(o1.title);
-      map.delete(o2.title);
-      expect(() => map.delete(o3.title)).not.to.throw();
-      expect(Array.from(map.values())).to.eql([]);
-    });
-
-    it(`has(o1.title)`, function () {
-      const {map, o1} = this;
-      const o3 = {title: 'c'};
+    it(`has()`, function () {
+      const {map, o1, o3} = this;
       expect(map.has(o1.title)).to.be.true;
       expect(map.has(o3.title)).to.be.false;
     });
 
     it(`clear()`, function () {
       const {map} = this;
-      map.clear();
-      expect(map.size).to.equal(0);
+      expect(map.clear()).to.be.undefined;
+      expect(Array.from(map.values())).to.eql([]);
     });
   });
 };
