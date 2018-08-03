7eeb5babea1b67b25081d14017e06c10e1877c85
Jason Lenoble <jason.lenoble@gmail.com>
2018-08-03 18:50:20 +0200

    Moved declarations inside describe

--- test/helpers/hides-parent-properties.js
+++ test/helpers/hides-parent-properties.js
@@ -6,11 +6,11 @@ import EventEmitter from 'events';
 export const hidesParentProperties = ({
   Type, ParentType, typeArgs = [], parentTypeArgs = [],
 }) => {
-  const obj = new Type(...typeArgs);
-  const parent = new ParentType(...parentTypeArgs);
-  const objKeys = Object.keys(obj);
-
   describe(`hides parent properties`, function () {
+    const obj = new Type(...typeArgs);
+    const parent = new ParentType(...parentTypeArgs);
+    const objKeys = Object.keys(obj);
+
     for (let key in parent) { // eslint-disable-line guard-for-in
       it(`.${key}`, function () {
         expect(objKeys).not.to.include(key);
