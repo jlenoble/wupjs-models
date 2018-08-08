05609b61ddc2b820e7a4e628a3ec974bba50a3da
Jason Lenoble <jason.lenoble@gmail.com>
2018-08-08 15:46:32 +0200

    Adopt items with consistent type and id

--- src/collection.js
+++ src/collection.js
@@ -4,19 +4,20 @@ import {Model as Class} from './model';
 
 export class Collection extends aggregation(Map, Item) {
   constructor (map = new Map(), {Model = Class} = {}) {
-    super(Array.from(map).map(([key, obj]) => {
-      return [key, new Model({...obj})];
-    }));
+    super();
 
     Object.defineProperty(this, 'Model', {value: Model});
+
+    Array.from(map).forEach(([key, obj]) => this.set(key, obj));
   }
 
   set (_id, item) {
     if (this.has(_id)) {
       this.get(_id).item = item;
+    } else if (item instanceof this.Model && item._id === _id) {
+      super.set(_id, item);
     } else {
-      const Model = this.Model || item.constructor; // necessary on init
-      super.set(_id, new Model({...item, _id}));
+      super.set(_id, new this.Model({...item, _id}));
     }
 
     return this;
