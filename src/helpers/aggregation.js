65e94fbded187adfd89f2f4013ea516724935408
Jason Lenoble <jason.lenoble@gmail.com>
2018-08-09 01:15:14 +0200

    Don't overwrite already defined props
    Especially important as they may be overridden up the
    inheritance chain.

--- src/helpers/aggregation.js
+++ src/helpers/aggregation.js
@@ -1,6 +1,7 @@
 // Adapted from:
 // https://stackoverflow.com/questions/29879267/es6-class-multiple-inheritance
 // Added recursion uo the prototype chain
+// Added no overwrite
 
 const copyProps = (target, source) => {
   // this function copies all properties and symbols, filtering out some
@@ -9,7 +10,8 @@ const copyProps = (target, source) => {
   Object.getOwnPropertyNames(source)
     .concat(Object.getOwnPropertySymbols(source))
     .forEach(prop => {
-      if (!prop.match(/^(?:constructor|prototype|arguments|caller|name|bind|call|apply|toString|length)$/)) {
+      if (!prop.match(/^(?:constructor|prototype|arguments|caller|name|bind|call|apply|toString|length)$/)
+        && !target[prop]) { // Don't overwrite if already overloaded
         Object.defineProperty(target, prop,
           Object.getOwnPropertyDescriptor(source, prop));
       }
