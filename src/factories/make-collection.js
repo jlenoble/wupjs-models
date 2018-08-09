6fe443d9cf3d278e0c9bec0c2553478b5be1fa09
Jason Lenoble <jason.lenoble@gmail.com>
2018-08-09 12:28:57 +0200

    Added static variable Model to Class

--- src/factories/make-collection.js
+++ src/factories/make-collection.js
@@ -1,10 +1,16 @@
 import {Collection} from '../collection';
 import {collectionClassName, makeClassFactory} from '../helpers';
 
-const classImpl = (name, Model) => class extends Collection {
-  constructor (item, {context} = {}) {
-    super(item, {Model, context});
-  }
+const classImpl = (name, Model) => {
+  const Class = class extends Collection {
+    constructor (item, {context} = {}) {
+      super(item, {Model, context});
+    }
+  };
+
+  Class.Model = Model;
+
+  return Class;
 };
 
 export const makeCollection = makeClassFactory(collectionClassName, classImpl);
