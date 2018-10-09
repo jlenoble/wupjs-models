2f1c9e1567218a198a1af6e49bf8311e57d52449
Jason Lenoble <jason.lenoble@gmail.com>
2018-10-09 23:28:39 +0200

    Exporting factories

---
+++ src/factories/index.js
@@ -0,0 +1,4 @@
+export {makeCollection} from './make-collection';
+export {makeModel} from './make-model';
+export {makeProperty} from './make-property';
+export {makeValidator} from './make-validator';
--- src/index.js
+++ src/index.js
@@ -2,3 +2,4 @@ export * from './collection';
 export * from './item';
 export * from './model';
 export * from './property';
+export * from './factories';
