bc14a09f996abe0494929400e543577afd69b04d
Jason Lenoble <jason.lenoble@gmail.com>
2018-10-11 20:58:09 +0200

    Reintegrated collection factories

--- src/collections.js
+++ src/collections.js
@@ -1,4 +1,29 @@
+import {Collection} from './collection';
 import models from './models';
-import {makeCollections} from './helpers/make-collections';
+import {makeDefaultExport} from './helpers/make-default-export';
+import {instanceName, collectionClassName} from './helpers/make-name';
+import {makeClassFactory} from './helpers/make-class-factory';
+
+const classImpl = (name, Model) => {
+  const Class = class extends Collection {
+    constructor (item, {context} = {}) {
+      super(item, {Model, context});
+    }
+  };
+
+  Class.Model = Model;
+
+  return Class;
+};
+
+const makeCollection = makeClassFactory(collectionClassName, classImpl);
+
+const makeCollections = models => makeDefaultExport(
+  models.byName,
+  makeCollection,
+  Class => Class.name,
+  Class => Class,
+  instanceName
+);
 
 export default makeCollections(models);
