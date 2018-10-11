0fc182eba7e3cdc406bab541b4a67354044be8a5
Jason Lenoble <jason.lenoble@gmail.com>
2018-10-11 20:45:41 +0200

    Reintegrated property factories

--- src/properties.js
+++ src/properties.js
@@ -1,4 +1,23 @@
+import {Property} from './property';
 import {propertyValidators} from './validators';
-import {makeProperties} from './helpers/make-properties';
+import {makeDefaultExport} from './helpers/make-default-export';
+import {instanceName, className} from './helpers/make-name';
+import {makeClassFactory} from './helpers/make-class-factory';
+
+const classImpl = (name, validator) => class extends Property {
+  constructor (item, {context, events, options} = {}) {
+    super(item, {name, context, validator, events, options});
+  }
+};
+
+const makeProperty = makeClassFactory(className, classImpl);
+
+const makeProperties = validators => makeDefaultExport(
+  validators.byName,
+  makeProperty,
+  Class => Class.name,
+  Class => Class,
+  instanceName
+);
 
 export default makeProperties(propertyValidators);
