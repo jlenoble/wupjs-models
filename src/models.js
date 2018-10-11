93c3482096c8e07b043b7e3476b4ce61181eba31
Jason Lenoble <jason.lenoble@gmail.com>
2018-10-11 20:51:07 +0200

    model factories reintegrated

--- src/models.js
+++ src/models.js
@@ -1,4 +1,37 @@
+import {Model} from './model';
 import {modelValidators} from './validators';
-import {makeModels} from './helpers/make-models';
+import {makeDefaultExport} from './helpers/make-default-export';
+import {instanceName, className} from './helpers/make-name';
+import {makeClassFactory} from './helpers/make-class-factory';
+import {makeEvents} from './helpers/make-events';
+
+const classImpl = (name, validator) => {
+  const Class = class extends Model {
+    constructor (item, {context} = {}) {
+      super(item, {validator, context});
+    }
+  };
+
+  const props = new Set(Object.keys(validator.constructor.schemaOptions));
+
+  if (!props.has('_id')) {
+    props.add('_id');
+  }
+
+  Class.props = props;
+  Class.events = makeEvents(Class, name);
+
+  return Class;
+};
+
+const makeModel = makeClassFactory(className, classImpl);
+
+const makeModels = validators => makeDefaultExport(
+  validators.byName,
+  makeModel,
+  Class => Class.name,
+  Class => Class,
+  instanceName
+);
 
 export default makeModels(modelValidators);
